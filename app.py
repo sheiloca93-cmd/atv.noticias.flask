a12cd2d92bb62daa93912cd198c88a07be5a1d2f
sheiloca93-cmdgit config --global user.email sheiloca93@gmail.com <sheiloca93@gmail.com>
2025-12-19 23:02:45 -0300

    implementar rota post noticia

--- app.py
+++ app.py
@@ -5,22 +5,78 @@ app = Flask(__name__)
 
 noticias = [
     {
-      "titulo": "Notícia 1",
-        "conteudo": "Conteúdo da notícia 1"
+      "id": 1, 
+        "nome": "Conteúdo da notícia 1"
     },
     {
-      "titulo": "Notícia 2",
-        "conteudo": "Conteúdo da notícia 2"
+      "id": 2,
+        "nome": "Conteúdo da notícia 2"
     },
     {
-      "titulo": "Notícia 3",
-        "conteudo": "Conteúdo da notícia 3"
+      "id": 3,
+        "nome": "Conteúdo da notícia 3"
     }
 ]
 
 @app.get('/noticias')
 def get_noticias():
-    return {"noticias": noticias}
+    return jsonify({"Noticias": noticias})
+
+@app.get('/noticia/<int:id>')
+def get_noticia_by_id(id):
+  
+  @app.get('/noticia')
+  def get_noticia():
+     name = request.args.get('nome')
+
+
+  for noticia in noticias:
+        if noticia["nome"] == name:
+           
+         if noticias["id"] == id:
+            return jsonify(noticia), 200
+        
+  return jsonify({"erro": "Notícia não encontrada"}), 404
+
+
+@app.post('/noticia')
+def add_noticia():
+    nova_noticia = request.get_json()
+    noticia = {
+        "id": len(noticias) + 1,
+        "nome": nova_noticia["nome"]
+    }
+    
+
+    noticias.append(noticia)
+    return jsonify(noticia), 201
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+        
+
+        
+
+
+        
+
+        
+  
+    
+    
+   
+
+
+
 
 
 
